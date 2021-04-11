5ca254e358ff8f152ba26c990028879a707ca925
Jaime Dantas <jaimejales@hotmail.com>
2021-04-11 13:31:02 -0300

    Added new load test

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,49 @@
-import time
-from locust import HttpUser, task, between
+from locust import HttpUser, TaskSet, task, constant
+from locust import LoadTestShape
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 2.5)
 
-    def on_start(self):
-        self.client.get("/resource/123e4567-e89b-12d3-a456-426614174000")
+class UserTasks(TaskSet):
+    @task
+    def get_root(self):
+        self.client.get("/")
+
+
+class WebsiteUser(HttpUser):
+    wait_time = constant(0.5)
+    tasks = [UserTasks]
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+    threshold = 120
+    before = 1
+    userIdle = 5
+    totalTestTime = 3600
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time < self.threshold:
+            tick_data = (self.userIdle, self.userIdle)
+            self.before = self.userIdle;
+            #print("run: "+str(run_time))
+            return tick_data
+        elif run_time < self.totalTestTime:
+            value = 0.015 + self.before
+            self.before = value
+            print("User: "+str(value))
+            #print("run: "+str(run_time))
+            tick_data = (int(value), int(value))
+            return tick_data
+
+        return None
